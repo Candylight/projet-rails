f19e64faab915e1940c0029c2d1e3a4e41f2d6ce
Candylight <benjam.hil@gmail.com>
2016-05-03 12:54:51 +0200

    Add associations to model

--- app/models/hs_card.rb
+++ app/models/hs_card.rb
@@ -3,4 +3,6 @@ class HsCard < ActiveRecord::Base
   belongs_to :rarity
   belongs_to :hs_class
   belongs_to :extension
+  belongs_to :type
+  belongs_to :group
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,3 @@
 class Type < ActiveRecord::Base
+  has_many :hs_cards
 end
